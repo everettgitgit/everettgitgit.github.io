f3d968223c4983ed49286a821c019595c95de234
Ethan Wai <144509132+everettgitgit@users.noreply.github.com>
2024-02-08 04:53:36 +0800

    Add files via upload

---
+++ Image gallery/script.js
@@ -0,0 +1,29 @@
+let images = [
+    "https://images.unsplash.com/photo-1682687219640-b3f11f4b7234?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1706463629335-d92264bbfd6f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1682695796497-31a44224d6d6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+];
+
+let currentIndex = 0;
+
+function updateImage() {
+    $('#image').attr('src', images[currentIndex]);
+}
+
+$('#prevButton').click(function() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    updateImage();
+});
+
+$('#nextButton').click(function() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    updateImage();
+});
+
+updateImage();
