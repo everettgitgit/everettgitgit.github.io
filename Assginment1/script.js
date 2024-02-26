bb531c5cc1cc5d20a2062b9db48dda7546950b01
Ethan Wai <144509132+everettgitgit@users.noreply.github.com>
2024-02-27 05:20:09 +0800

    Delete Assginment1 directory

--- Assginment1/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.getElementById('imageName').style.visibility = 'visible';
-    }, 10000);
-    document.getElementById('currentYear').textContent = new Date().getFullYear();
-};
